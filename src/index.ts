17ec029aa4241d1c668653e7018ea781868bd31b
Sulyok Csaba <sulyok.csaba@codespring.ro>
2022-01-20 14:58:38 +0200

    remove express

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import QWebSocket from './common/queuews';
-import expressQws from './node/decorateexpress';
 import QWebSocketServer from './node/decoratews';
 
 export * from './common/message';
 export * from './common/queuews';
 export * from './node/messageencode';
 
-export { expressQws };
 export { QWebSocketServer };
 
 export default QWebSocket;
